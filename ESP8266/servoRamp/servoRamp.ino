3b1a93e0fa7d8a711b602e73d118960fc8a620c1
Ismar Muslić <ismar.muslic@outlook.com>
2024-05-29 00:55:00 +0200

    Added last will message for connection loss + code readability update

--- ESP8266/servoRamp/servoRamp.ino
+++ ESP8266/servoRamp/servoRamp.ino
@@ -5,151 +5,82 @@
 #define LED_GREEN D1
 #define BUTTON D5
 
-
 Servo servo;
 
 EspMQTTClient client(
-  "Asterix_WLAN",
-  "123A456d78",
+  "Asterix_WLAN",       // WiFi SSID
+  "123A456d78",         // WiFi Password
   "broker.hivemq.com",  // MQTT Broker server ip
-  "TestClient",     // Client name that uniquely identify your device
-  1883              // The MQTT port, default to 1883. this line can be omitted
+  "TestClient",         // Client name that uniquely identify your device
+  1883                  // The MQTT port, default to 1883. this line can be omitted
 );
 
 // This function is called once everything is connected (Wifi and MQTT)
-// WARNING : YOU MUST IMPLEMENT IT IF YOU USE EspMQTTClient
-void onConnectionEstablished()
-{
-  client.publish("USprojekat/Konekcija", "Device Online.", true);
+void onConnectionEstablished() {
+  client.publish("USprojekat/Konekcija", "Device Online", true);
   client.publish("USprojekat/ZelenoSvjetlo", "0");
   client.publish("USprojekat/CrvenoSvjetlo", "1");
   
+  // Subscribe to "USprojekat/Servo" to work with all the functionalities
+  client.subscribe("USprojekat/Servo", 
+    [](const String & payload) {
+      Serial.println(payload);
 
+      if(payload == "1") {
+        // Set LEDs to GO
+        client.publish("USprojekat/ZelenoSvjetlo", "1");
+        client.publish("USprojekat/CrvenoSvjetlo", "0");
+        digitalWrite(LED_RED, LOW);
+        digitalWrite(LED_GREEN, HIGH);
 
-  // Subscribe to "mytopic/test" and display received message to Serial
-  client.subscribe("USprojekat/Servo", [](const String & payload) {
-    Serial.println(payload);
+        // Raise the ramp
+        Serial.println("Podizem rampu...");
+        client.publish("USprojekat/Info", "Podizem rampu...");
+        int pos = 0;
+        for(pos; pos <= 180; pos += 3) 
+          servo.write(pos);
 
-    if(payload == "1") {
-      Serial.println("Podizem rampu...");
-      client.publish("USprojekat/Info", "Podizem rampu...", true);
-      client.publish("USprojekat/ZelenoSvjetlo", "1");
-      client.publish("USprojekat/CrvenoSvjetlo", "0");
-      digitalWrite(LED_RED, LOW);
-      digitalWrite(LED_GREEN, HIGH);
-      int pos = 0;
-      for(pos; pos <= 180; pos += 3) 
-        servo.write(pos);
-      //Wait for car to pass
-      Serial.println("Rampa podignuta!");
-      client.publish("USprojekat/Info", "Rampa podignuta!", true);
-      delay(5000);
-      //Switch LEDs back from GO to STOP
-      client.publish("USprojekat/ZelenoSvjetlo", "0");
-      client.publish("USprojekat/CrvenoSvjetlo", "1");
-      digitalWrite(LED_RED, HIGH);
-      digitalWrite(LED_GREEN, LOW);
-      //Lower the ramp
-      Serial.println("Spustam rampu...");
-      client.publish("USprojekat/Info", "Spustam rampu...", true);
-      for(pos = 180; pos >= 0; pos -= 2) 
-        servo.write(pos); 
-      
-      Serial.println("Rampa spustena.");
-      client.publish("USprojekat/Info", "Rampa spustena.", true);
-      client.publish("USprojekat/Servo", "0");    
-    }
-  });
+        // Wait 5s for the car to pass
+        Serial.println("Rampa podignuta!");
+        client.publish("USprojekat/Info", "Rampa podignuta!");
+        delay(5000);
 
-  /*
-  // Subscribe to "mytopic/wildcardtest/#" and display received message to Serial
-  client.subscribe("USprojekat/wildcardtest/#", [](const String & topic, const String & payload) {
-    Serial.println("(From wildcard) topic: " + topic + ", payload: " + payload);
-  });
+        // Set LEDs to GO
+        client.publish("USprojekat/ZelenoSvjetlo", "0");
+        client.publish("USprojekat/CrvenoSvjetlo", "1");
+        digitalWrite(LED_RED, HIGH);
+        digitalWrite(LED_GREEN, LOW);
 
-  // Publish a message to "mytopic/test"
-  client.publish("USprojekat/test", "This is a message"); // You can activate the retain flag by setting the third parameter to true
-
-  // Execute delayed instructions
-  client.executeDelayed(5 * 1000, []() {
-    client.publish("USprojekat/wildcardtest/test123", "This is a message sent 5 seconds later");
-  });
-  */
+        // Lower the ramp
+        Serial.println("Spustam rampu...");
+        client.publish("USprojekat/Info", "Spustam rampu...");
+        for(pos = 180; pos >= 0; pos -= 2) 
+          servo.write(pos); 
+        
+        Serial.println("Rampa spustena.");
+        client.publish("USprojekat/Info", "Rampa spustena.");
+        client.publish("USprojekat/Servo", "0");    
+      }
+    });
 }
 
 void setup() {
-
   Serial.begin(115200);
 
-  // Optional functionalities of EspMQTTClient
-  client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
-  client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
-  client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
-  client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+  client.enableLastWillMessage("USprojekat/Konekcija", "Device Offline", true);  
 
+  servo.attach(D4);               // Set the pin for the servo
+  servo.write(0);                 // Set the servo to 0 position
+  pinMode(LED_RED, OUTPUT);       // Set the pin for STOP LED
+  pinMode(LED_GREEN, OUTPUT);     // Set the pin for GO LED
 
-  servo.attach(D4);               //Set the pin for the servo
-  servo.write(0);                 //Set the servo to 0 position
-  pinMode(LED_RED, OUTPUT);       
-  pinMode(LED_GREEN, OUTPUT);
-  pinMode(BUTTON, INPUT);
-  digitalWrite(LED_RED, HIGH);    //Set LEDs to STOP
+  // Set LEDs to STOP position on start
+  digitalWrite(LED_RED, HIGH);
   digitalWrite(LED_GREEN, LOW);
 }
 
 void loop() {
+  // Scan for new messages every 250ms
   client.loop();
-  delay(100);
-  /*
-
-  if(digitalRead(BUTTON) == HIGH)
-    //Debounce the button
-    if(buttonTime - previousButtonTime > 250) {
-      int pos = 0;
-      //Switch the LEDs from STOP to GO
-      digitalWrite(LED_RED, LOW);
-      digitalWrite(LED_GREEN, HIGH);
-
-      //Raise the ramp
-      for(pos; pos <= 180; pos += 5) 
-        servo.write(pos);
-      //Wait for car to pass
-      delay(5000);
-      //Switch LEDs back from GO to STOP
-      digitalWrite(LED_RED, HIGH);
-      digitalWrite(LED_GREEN, LOW);
-
-      //Lower the ramp
-      for(pos = 180; pos >= 0; pos -= 3) 
-        servo.write(pos);  
-      
-      //Update debouncing time
-      previousButtonTime = buttonTime;
-    }
-    
-
-
-    //////////////////////////////////////////////////////////
-    digitalWrite(LED_RED, LOW);
-    digitalWrite(LED_GREEN, HIGH);
-    int pos = 0;
-    for(pos; pos <= 180; pos += 5) 
-        servo.write(pos);
-      //Wait for car to pass
-      delay(2000);
-      //Switch LEDs back from GO to STOP
-      
-
-      //Lower the ramp
-      for(pos = 180; pos >= 0; pos -= 3) 
-        servo.write(pos); 
-      digitalWrite(LED_RED, HIGH);
-      digitalWrite(LED_GREEN, LOW);
-
-      delay(2000);
-      //////////////////////////////////////////////////////////
-
-      */
-
-
+  delay(250);
 }
