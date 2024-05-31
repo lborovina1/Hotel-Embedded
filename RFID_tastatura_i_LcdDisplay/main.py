e171e9a36a701522ef6f2c1b367a4589bb12653e
Ivona <ijozic1@etf.unsa.ba>
2024-05-31 17:41:20 +0200

    RFID funkcionira

--- RFID_tastatura_i_LcdDisplay/main.py
+++ RFID_tastatura_i_LcdDisplay/main.py
@@ -8,14 +8,32 @@ I2C_ADDR     = 63
 I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
 
+reader1 = MFRC522(spi_id=0,sck=2,miso=4,mosi=3,cs=1,rst=5) # cs = sda
+reader2 = MFRC522(spi_id=1,sck=10,miso=12,mosi=11,cs=9,rst=13)
+
 soba = 0
 tastatura = TastController()
 debounce = ticks_ms()
        
 button = Pin(0, Pin.IN)
 
+red = Pin(7, Pin.OUT)
+green = Pin(6, Pin.OUT)
+
 i2c = I2C(1, sda=Pin(14), scl=Pin(15), freq=400000)
 lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)
+
+karticeISobe = {
+  1000: 0,
+  1001: 0,
+  1002: 0,
+  2000: 0,
+  2001: 0,
+  2002: 0,
+  3000: 0,
+  3001: 0,
+  3002: 0
+}
 
 def unosSobe():
     global soba, sobaUnesena, debounce, tastatura
@@ -40,5 +58,38 @@ while True:
             lcd.putstr("Kliknite taster\ni unesite opet")
         else:
             lcd.putstr("Unijeli ste: \n" + soba)  
-            #sleep(3) 
+            sleep(2)
+            lcd.clear()
+            lcd.putstr("Prislonite\nkarticu!: \n") 
+            reader1.init()
+            stat1 = False
+            #while stat1 == False:
+            sleep(2)
+            lcd.clear()
+            (stat1, tag_type1) = reader1.request(reader1.REQIDL)
+            
+            if stat1 == reader1.OK:
+                (stat, uid) = reader1.SelectTagSN()
+                if stat == reader1.OK:
+                    card = int.from_bytes(bytes(uid),"little",False) # type: ignore
+                    karticeISobe[int(soba)] = card 
+                    print(karticeISobe[int(soba)])
+            lcd.putstr("Povezano!")
+
+    else:
+        reader2.init()
+        (stat2, tag_type2) = reader2.request(reader2.REQIDL)
+   
+        if stat2 == reader2.OK:
+            (stat, uid) = reader2.SelectTagSN()
+            if stat == reader2.OK:
+                card = int.from_bytes(bytes(uid),"little",False)
+                print(card)
+                print(karticeISobe[1000])
+                if karticeISobe[1000] == card:
+                    red.value(0)
+                    green.value(1)
+                else:
+                    red.value(1)
+                    green.value(0)
     sleep(0.1)
